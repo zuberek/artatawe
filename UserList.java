14c35d2e72866719d08619c62bb906e13008b5f7
Jasiudab <dabrowski.janek@gmail.com>
2017-11-28 15:57:54 +0000

    Adding a commit

--- UserList.java
+++ UserList.java
@@ -1,3 +1,5 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -6,13 +8,33 @@ import java.util.ArrayList;
  *
  */
 public class UserList {
-
-	public ArrayList<User> users;
 	
+	ArrayList<Bid> userList;
+	DB db;
 	
-	public UserList(){
-		
+	public UserList() {
+		db = new DB();
 	}
+	
+//	/**
+//	 * Returns an ArrayList of User objects that then can be used to populate a listview.
+//	 * 
+//	 * @param userId
+//	 * @return an ArrayList of all bids specified user has made
+//	 */
+//	public ArrayList<Bid> getUserBidList(int userId) {
+//		bidList = new ArrayList<Bid>();
+//		try {
+//			ResultSet rs = db.select("SELECT * from `bids` WHERE `bidderID` = '" + userId + "'");
+//			while (rs.next()) {
+//				Bid bid = new Bid(rs.getInt("bidID"));
+//				bidList.add(bid);
+//	        }
+//		} catch (SQLException ex) {
+//			ex.getMessage();
+//		}
+//		return bidList;
+//	}
 	
 
 }
