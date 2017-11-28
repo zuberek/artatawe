d988903378668a437837401c8d4f29f5bff43eae
921340 <921340@ISS-CZC73075KH.tawe.swan.ac.uk>
2017-11-28 14:27:52 +0000

    Added constructor

--- UserList.java
+++ UserList.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
 
+/**
+ * This class is used to generate ArrayList's of User objects
+ * @author Joshua Blackman
+ *
+ */
 public class UserList {
+
+	public ArrayList<User> users;
+	
+	
+	public UserList(){
+		
+	}
+	
 
 }
