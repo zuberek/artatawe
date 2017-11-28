a3c694b1dd5e50344bbde387655d519adc301854
Joshua Blackman <joshuadennisblackman@gmail.com>
2017-11-28 05:51:28 +0000

    Added connect function for Database.

--- DB.java
+++ DB.java
@@ -1,4 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DB {
+	
+	Connection conn = null;
+	
+	public DB() {
+		connect();
+	}
+	
+	
+	public void connect() {        
+        try {
+            String url = "jdbc:sqlite:artatawe.db";
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connected to SQLLite DB.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+	
+	
 
+    
 }
