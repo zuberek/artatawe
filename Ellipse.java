6d13683a500ff5b369dc8b95c771db69f740515c
djsaunders1997 <34069335+djsaunders1997@users.noreply.github.com>
2017-11-28 22:37:37 +0000

    Update Ellipse.java
    Added basic attributes and constructor

--- Ellipse.java
+++ Ellipse.java
@@ -1,4 +1,22 @@
-
-public class Ellipse {
-
+public class Ellipse extends ShapeElement{
+    private int width;
+    private int height;    
+    
+     /**
+     *
+     * @param width the width of the ellipse
+     * @param height the height of the ellipse     
+     * @param isFilled whether the shape will be filled or solid
+     * 
+     * *  if width and height are the same then the ellipse is a circle
+     */
+    public Ellipse(int width, int height, boolean isFilled){
+        super(isFilled);        //will depend on the parameters constructor of the superclass
+        this.width = width;
+        this.height = height;
+    }
+    
+    public void draw(){
+        //depends on the implimentation of the canvas
+    }
 }
