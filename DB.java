b4510f3bb5cc91f3ba6440640466b9cc5590f478
Joshua Blackman <joshuadennisblackman@gmail.com>
2017-11-28 07:34:33 +0000

    Added init class to setup database

--- DB.java
+++ DB.java
@@ -1,38 +1,52 @@
-/*
- * @author Joshua Blackman
- */
+
 
 import java.sql.*;
+import java.util.ArrayList;
 
+/**
+ * This class handles connecting to the database and retrieving data.
+ * @author Joshua Blackman
+ */
 public class DB {
 	
 	Connection conn = null;
 	
 	public DB() {
 		connect();
+		init();
 	}
 	
-	
-	public void connect() {        
+	private void connect() {        
         try {
             String connectionUrl = "jdbc:sqlite:artatawe.db";
-            conn = DriverManager.getConnection(connectionUrl);
-            
-            System.out.println("Connected to SQLLite DB.");
-            
+            conn = DriverManager.getConnection(connectionUrl);        
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
         }
     }
 	
+	/**
+	 * This method sets up all the tables
+	 */
+	private void init() {
+		String sql = "CREATE TABLE IF NOT EXISTS `user` (\r\n" + 
+				" `userID` INTEGER PRIMARY KEY,\r\n" + 
+				" `userName` text NOT NULL,\r\n" + 
+				" `firstName` text NOT NULL,\r\n" + 
+				" `lastName` text NOT NULL,\r\n" + 
+				" `phoneNo` int(11) NOT NULL,\r\n" + 
+				" `userAddress` text NOT NULL,\r\n" + 
+				" `lastLogin` int(11) NOT NULL\r\n" + 
+				");";
+		
+		
+		query(sql);
+	}
+	
+	/**
+	 * This method is used for executing a query on the database.
+	 * @param query
+	 */
 	public void query(String query) {
 		try {
 			Statement statement = conn.createStatement();
@@ -41,6 +55,16 @@ public class DB {
 		} catch(SQLException ex) {
 			System.out.println(ex.getMessage());
 		}
+	}
+	
+	
+	/**
+	 * 
+	 * @return ArrayList of objects from the select query
+	 */
+	public ArrayList<Object> select() {
+		return null;
+		
 	}
 
     
