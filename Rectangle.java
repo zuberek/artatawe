989349aecc92c575a09d72e53cdf156d836cb8fe
djsaunders1997 <34069335+djsaunders1997@users.noreply.github.com>
2017-11-28 22:34:53 +0000

    Update Rectangle.java
    Added basic attributes and constructor

--- Rectangle.java
+++ Rectangle.java
@@ -1,4 +1,24 @@
-
-public class Rectangle {
-
+/**
+ * creates a Rectangle object
+ * @author David 
+ */
+public class Rectangle extends ShapeElement{
+    private int width;
+    private int height;
+    
+    /**
+     *
+     * @param width the width of the rectangle
+     * @param height the height of the rectangle
+     * @param isFilled whether the shape will be filled or solid 
+     */
+    public Rectangle(int width, int height, boolean isFilled){   
+        super(isFilled);        //will depend on the parameters constructor of the superclass
+        this.width = width;
+        this.height = height;        
+    }
+    
+    public void draw(){
+        //depends on the implimentation of the canvas
+    }
 }
