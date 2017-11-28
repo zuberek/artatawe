e68a592cf4e94dcee19279f43ff511809e682d7f
Jasiudab <dabrowski.janek@gmail.com>
2017-11-28 14:32:59 +0000

    Edit variables

--- User.java
+++ User.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
 
 public class User {
+	
+	private String userName;
+	
+	private String firstName;
+	private String lastName;
+	private String phoneNo;
+	private String userAddress;
+	private int userID;
+	private int unixLoginTStamp;
+	private int unixLogoutTstamp;
+	private Avatar profilePicture;
+	private ArrayList<User>  favouriteUser;
 
 	public User(){
 		
