b0f7beb0775ee98443a948ec774e196379ccabad
Joshua Blackman <joshuadennisblackman@gmail.com>
2017-12-01 05:43:07 +0000

    Created Login.java
    - Added basic constructor for scene
    - Added main to this class

---
+++ Login.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+	public static void main(String[] args) {
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+	    
+        Scene scene = new Scene(root);
+        
+        
+        stage.setTitle("Artatawe | Login");
+        stage.setScene(scene);
+        stage.show();		
+	}
+
+}
