03b919ddcfe7c877efca24fc311317bc573122b3
HefinOJ <33953672+HefinOJ@users.noreply.github.com>
2017-12-01 12:59:54 +0000

    Update Line.java

--- Line.java
+++ Line.java
@@ -1,4 +1,28 @@
+/**
+ * Creates a Line object
+ * @author Hefin Johnson
+ */
 
-public class Line {
+public class Line extends LinearElement{
+    private int width;
+    private int height;
 
+    /**
+     *
+     * @param Width will set the width of the Line
+     * @param Height will set the height of the Line
+     */
+    
+    Public Line(int width, int height){
+        this.width = width;
+        this.height = height;
+        
+    }
+
+  
+    public void draw(){
+        
+        // Dependant on how the canvas is implemented
+    
+    }
 }
