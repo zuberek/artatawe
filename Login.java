0cf92714dba0f413ae317f65388b18ee42496443
Joshua Blackman <joshuadennisblackman@gmail.com>
2017-12-01 05:44:31 +0000

    Update Login.java
    - Removed redundant imports

--- Login.java
+++ Login.java
@@ -1,12 +1,8 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+
 import javafx.stage.Stage;
 
 public class Login extends Application {
@@ -19,8 +15,7 @@ public class Login extends Application {
 		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
 	    
         Scene scene = new Scene(root);
-        
-        
+
         stage.setTitle("Artatawe | Login");
         stage.setScene(scene);
         stage.show();		
