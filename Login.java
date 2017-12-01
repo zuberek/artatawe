16067a07c001bfca9e00d13d118fb1b98fd28533
Joshua Blackman <joshuadennisblackman@gmail.com>
2017-12-01 05:49:45 +0000

    Update Login.java
    - Updated javadoc

--- Login.java
+++ Login.java
@@ -5,11 +5,23 @@ import javafx.scene.Scene;
 
 import javafx.stage.Stage;
 
+/**
+ * This the main class, first  point of contact for the user.
+ * It is used to open u
+ * @author  Joshua Blackman
+ *
+ */
 public class Login extends Application {
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
         launch(args);
     }
 
+	/* (non-Javadoc)
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
 	@Override
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
