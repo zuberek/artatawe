2bc23ca40c3ef6adcea1c607aa38fcf9e5187768
Jasiudab <dabrowski.janek@gmail.com>
2017-12-03 01:00:01 +0000

    Simplifying BidList
    By adding a method populateArray(String query, Array <Bid>) I reduce the repetition in the code

--- src/BidList.java
+++ src/BidList.java
@@ -29,16 +29,9 @@ public class BidList {
 	 */
 	public ArrayList<Bid> getUserBidList(int userId) {
 		bidList = new ArrayList<Bid>();
-		try {
-			ResultSet rs = db.select("SELECT * from `bids` WHERE `bidderID` = '" + userId + "'");
-			while (rs.next()) {
-				Bid bid = new Bid(rs.getInt("bidID"));
-				bidList.add(bid);
-	        }
-		} catch (SQLException ex) {
-			ex.getMessage();
-		}
-		db.closeQuietly();
+
+		String query = "SELECT * from `bids` WHERE `bidderID` = '" + userId + "'";
+		populateArray(query, bidList);
 		return bidList;
 	}
 	
@@ -50,15 +43,39 @@ public class BidList {
 	 */
 	public ArrayList<Bid> getAuctionBidList(int auctionID){
 		bidList = new ArrayList<Bid>();
+
+		String query = "SELECT * from `bids` WHERE `auctionID` = '" + auctionID + "'";
+		populateArray(query, bidList);
+		
+		return bidList;
+	}
+
+	/**
+	 * Returns an ArrayList of Bid objects that then can be used to populate a listview.
+	 *
+	 * @param userId
+	 * @return an ArrayList of all bids specified user has made
+	 */
+	public ArrayList<Bid> getUserHighestBids(int userId) {
+		bidList = new ArrayList<Bid>();
+
+		String query = "SELECT * from `bids` WHERE `bidderID` = '" + userId + "'" +
+				"GROUP BY `bidderID` HAVING MAX(`amount`) ORDER BY (`amount`)";
+		populateArray(query, bidList);
+
+		return bidList;
+	}
+
+	private ArrayList<Bid> populateArray(String query, ArrayList<Bid> bidList) {
 		try {
-			ResultSet rs = db.select("SELECT * from `bids` WHERE `auctionID` = '" + auctionID + "'");
+			ResultSet rs = db.select(query);
 			while (rs.next()) {
-				Bid bid = new Bid(rs.getInt("bidID"));
-				bidList.add(bid);
-	        }
+					Bid bid = new Bid(rs.getInt("bidID"));
+					bidList.add(bid);
+			}
 		} catch (SQLException ex) {
-			ex.getMessage();
-		}
+				ex.getMessage();
+			}
 		db.closeQuietly();
 		return bidList;
 	}
