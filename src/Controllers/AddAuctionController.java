9ee0261de8f1fccdb8dd73030a3a1d40776f0948
Joshua Blackman <joshuadennisblackman@gmail.com>
2017-12-06 17:49:01 +0000

    Add AddAuctionController.java
    - Added basic constructor

---
+++ src/Controllers/AddAuctionController.java
@@ -0,0 +1,14 @@
+package src.Controllers;
+
+import src.User;
+
+public class AddAuctionController {
+
+	User currentUser;
+	
+	public void initialize(User currentUser) {
+		// TODO Auto-generated method stub
+		this.currentUser = currentUser;
+	}
+
+}
