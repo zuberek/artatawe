ae332a959dfc4a026f65c2be65aba98f53faf836
Joshua Blackman <joshuadennisblackman@gmail.com>
2017-12-06 17:52:20 +0000

    Update Painting.java
    - Updated Consturctor
    - Added DB save method

--- src/Painting.java
+++ src/Painting.java
@@ -3,14 +3,20 @@ package src;
 public class Painting extends Artwork {
     private String paintingType;
 
-    public Painting(String title, String artist, String description, String photographPath, String dateCreated, double height, double width) {
-        setTitle(title);
+    public Painting(int userID, String title, String artist, String description, String photographPath, String dateCreated, double height, double width) {
+        setUserID(userID);
+    	setTitle(title);
         setArtist(artist);
         setDescription(description);
         setPhotographPath(photographPath);
         setDateCreated(dateCreated);
         setDimensions(height, width);
         setPaintingType(paintingType);
+        savePainting();
+    }
+    
+    public void savePainting() {
+    	DB.query("INSERT INTO `artwork` (`userID`, `artist`, `title`, `width`, `height`, `paintingType`, `dateCreated`) VALUES ('"+ this.getUserID() + "',  '" + this.getArtist() + ", '" + this.getDimensions()[1] + "', '" + this.getDimensions()[0] + "', '" + this.getPaintingType() + "', '" + this.getDateCreated() +"');");
     }
 
     /**
