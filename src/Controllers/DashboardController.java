347da519af01013287372ea55e1e4fa32e874207
Joshua Blackman <joshuadennisblackman@gmail.com>
2017-12-06 17:51:01 +0000

    Update DashboardController.java
    - Added event handlers for buttons

--- src/Controllers/DashboardController.java
+++ src/Controllers/DashboardController.java
@@ -1,15 +1,22 @@
 package src.Controllers;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import src.Auction;
 import src.AuctionList;
+import src.Login;
 import src.User;
 
 public class DashboardController {
@@ -22,7 +29,7 @@ public class DashboardController {
 	@FXML Label welcomeLabel;
 	@FXML ListView<String> browseAuctions;
 	
-	User currentUser = new User();
+	User currentUser;
 	ArrayList<Auction> auctions;
 	
 	public void initialize(User currentUser){
@@ -35,8 +42,48 @@ public class DashboardController {
 		
 	}
 
-	@FXML public void browseAuctionClicked(MouseEvent arg0) {
+	public void browseAuctionClicked(MouseEvent arg0) {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Scenes/ViewAuction.fxml"));
+			Parent editRoot = (Parent) fxmlLoader.load();
+	
+			ViewAuctionController ctrl = fxmlLoader.getController();
+			Auction auction =  new Auction(Integer.parseInt(browseAuctions.getSelectionModel().getSelectedItem()));
+			ctrl.initialize(currentUser, auction);
+	
+			Scene newScene = new Scene(editRoot);
+            Stage stage = new Stage();
+            stage.setScene(newScene);
+            stage.setTitle("Artatawe |  View Auction");
+
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	    System.out.println("clicked on " + browseAuctions.getSelectionModel().getSelectedItem());
+	}
+	
+	public void sellArtworkClicked() {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Scenes/AddAuction.fxml"));
+			Parent editRoot = (Parent) fxmlLoader.load();
+	
+			AddAuctionController ctrl = fxmlLoader.getController();
+			ctrl.initialize(this.currentUser);
+	
+			Scene newScene = new Scene(editRoot);
+            Stage stage = new Stage();
+            stage.setScene(newScene);
+            stage.setTitle("Artatawe | Add new Auction");
+
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
