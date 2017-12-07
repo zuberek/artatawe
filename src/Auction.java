04dec27ab59733850842ca8fff1470dfe3f06007
Joshua Blackman <joshuadennisblackman@gmail.com>
2017-12-07 17:31:45 +0000

    Update Auction.java
    - THis being commented  out has wasted like 300 hours of  my time trying
    to work out why it isn't work xD

--- src/Auction.java
+++ src/Auction.java
@@ -32,7 +32,7 @@ public class Auction {
 		setSellerID(sellerID);
 		setMaxBids(maxBids);
 		setReservePrice(reservePrice);
-		setLastBidID(-1);
+		setLastBidID(0);
 		setArtwork(artwork);
 		saveAuction();
 	}
@@ -52,7 +52,7 @@ public class Auction {
 				setDescription(rs.getString("description"));
 				setMaxBids(rs.getInt("maxBids"));
 				setReservePrice(rs.getDouble("reservePrice"));
-				//setLastBidID(rs.getInt("lastBidID"));
+				setLastBidID(rs.getInt("lastBidID"));
 				//setTimeAdded(rs.getDate(("timeAdded")));		
 				
 				artworkID = rs.getInt("artworkID");
