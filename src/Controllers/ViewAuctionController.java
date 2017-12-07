a9c76f377b62ef24892667780573f9212c4c314e
Joshua Blackman <joshuadennisblackman@gmail.com>
2017-12-07 00:19:52 +0000

    Update ViewAuctionController.java
    - Added event handler for viewSellerProfile

--- src/Controllers/ViewAuctionController.java
+++ src/Controllers/ViewAuctionController.java
@@ -2,18 +2,25 @@ package src.Controllers;
 
 
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import src.Auction;
 import src.Bid;
 import src.CONSTANTS;
+import src.Login;
 import src.User;
 
 /**
@@ -46,6 +53,30 @@ public class ViewAuctionController {
 		currentBidLabel.setText(String.valueOf(currentBid.getAmount()));
 	}
 	
-	
+	public void viewSellerProfile() {
+        try {
+        	
+        	User userToView = new User(auction.getSellerID());
+        	
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Scenes/ViewProfile.fxml"));
+            Parent editRoot = (Parent) fxmlLoader.load();
+
+            ViewProfileController vcp = fxmlLoader.getController();
+            vcp.initialize(userToView, currentUser);
+
+            Scene newScene = new Scene(editRoot);
+            Stage editStagee = new Stage();
+            editStagee.setScene(newScene);
+            editStagee.setTitle("Artatawe | View Profile");
+
+            editStagee.initModality(Modality.APPLICATION_MODAL);
+
+            editStagee.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Quit the program (with an error code)
+            System.exit(-1);
+        }
+	}
 
 }
