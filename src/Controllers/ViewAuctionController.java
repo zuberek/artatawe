9235bf69c68062cc16fcfe8eac87e75b0442e9f5
JDBlackman <joshuadennisblackman@gmail.com>
2017-12-10 17:43:44 +0000

    Update ViewAuctionController.java
    - Fixed merge errors

--- src/Controllers/ViewAuctionController.java
+++ src/Controllers/ViewAuctionController.java
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-package src.Controllers;
-=======
 package src.Controllers;
 
 
@@ -109,109 +106,7 @@ public class ViewAuctionController {
         }
 
 	}
-	
-	public void submitBid() {
-		float bid;
->>>>>>> 1a675332423d6251d7f0ca7ed8a1892066417000
 
-
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import src.Auction;
-import src.Bid;
-import src.CONSTANTS;
-import src.Login;
-import src.User;
-
-/**
- * @author Borislav Koynin
- * @author Joshua Blackman
- *
- */
-public class ViewAuctionController {
-	
-
-	@FXML VBox rootPane;
-	@FXML ImageView auctionImage;
-	@FXML Label auctionNameLabel;
-	@FXML TextArea auctionDescriptionTextBox;
-	@FXML Label currentBidLabel;
-	@FXML Label reservePrice;
-	@FXML Label bidsPlacedLabel;
-	@FXML TextField bidAmount;
-	
-	
-	Auction auction;
-	User currentUser;
-	
-	/**
-	 * Set the auction that is being edited.
-	 * When this window is closed, the changes will be set in this auction object.
-	 * @param currentUser user to change and initialise
-	 * @param auction auction to be altered
-	 */
-	public void initialize(User currentUser, Auction auction) {
-		this.currentUser = currentUser;
-		this.auction = auction;
-		
-		auctionNameLabel.setText(auction.getArtwork().getTitle());
-		auctionDescriptionTextBox.setText(auction.getArtwork().getDescription());
-		reservePrice.setText("£"+auction.getReservePrice());
-		bidsPlacedLabel.setText(auction.getCurrentBids(auction.getAuctionID()) + "/" + auction.getMaxBids());
-		InputStream stream = getClass().getResourceAsStream(auction.getArtwork().getPhotographPath());
-		Image newImage = new Image(stream);
-		auctionImage.setImage(newImage);
-		
-		
-		Bid currentBid = new Bid(auction.getLastBidID());
-		currentBidLabel.setText("£"+String.valueOf(currentBid.getAmount()));
-	}
-	
-
-	public void viewSellerProfile() {
-        try {
-        	
-        	User userToView = new User(auction.getSellerID());
-        	
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Scenes/ViewProfile.fxml"));
-            Parent editRoot = (Parent) fxmlLoader.load();
-
-            ViewProfileController vcp = fxmlLoader.getController();
-            vcp.initialize(userToView, currentUser);
-
-            Scene newScene = new Scene(editRoot);
-            Stage editStagee = new Stage();
-            editStagee.setScene(newScene);
-            editStagee.setTitle("Artatawe | View Profile");
-
-            editStagee.initModality(Modality.APPLICATION_MODAL);
-
-            editStagee.showAndWait();
-        } catch (IOException e) {
-            e.printStackTrace();
-            // Quit the program (with an error code)
-            System.exit(-1);
-        }
-
-	}
-	
 	public void submitBid() {
 		float bid;
 
